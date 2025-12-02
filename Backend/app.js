1cb389500bdaf12d801486a1ca0d5d4ee3ebed31
MOHAN P <152972970+MOHAN-MOORTHI@users.noreply.github.com>
2025-12-02 15:41:48 +0530

    worked on authentication

--- Backend/app.js
+++ Backend/app.js
@@ -1,13 +1,24 @@
 import express from "express"; // import express
 import cors from "cors";
+import cookieParser from "cookie-parser";
+import { errorHandler, notFound } from "./Middleware/error.middleware.js";
+import authRoute from "./Routes/auth.route.js";
 const app = express();
+
+//cors
+app.use(cors());
 
 // json body
 app.use(express.json());
 
-//cors 
-app.use(cors());
+// cookieParser
+app.use(cookieParser());
 
+// Routes
+app.use("api/v1/auth", authRoute);
 
+// Error Middleware
+app.use(errorHandler);
+app.use(notFound);
 
 export default app;
--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";  // encryption the date using this package
 
 const userSchema = mongoose.Schema(
   {
@@ -33,6 +34,20 @@ const userSchema = mongoose.Schema(
     timestampes: true,
   }
 );
+
+// user schema encryption
+userSchema.pre("save", async function (next) {
+  if(!this.isModified("password")){
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  
+});
+
+userSchema.methods.matchPassword = async function(enteredPassword){
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 const User = mongoose.model("User", userSchema);
 
